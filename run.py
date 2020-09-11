88d50be14cec327e18572baafb4dd8441a46e87d
DriptaSenapati <driptasenapati97@gmail.com>
2020-09-11 21:21:02 +0530

    file upload

---
+++ run.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, send_from_directory
+from cachebuster import CacheBuster
+
+app = Flask(__name__)
+config = {'extensions': ['.js', '.css', '.png', '.jpg'], 'hash_size': 5}
+
+cache_buster = CacheBuster(config=config)
+
+cache_buster.init_app(app)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/<path:filename>', methods=['GET'])
+def getpdf(filename):
+    return send_from_directory(app.static_folder, f'{filename}.pdf')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
