c276f45747c5a38170f1d0300e2e719e5bad548d
DriptaSenapati <driptasenapati97@gmail.com>
2020-10-25 20:23:22 +0530

    routes added

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -3,11 +3,20 @@ import secrets
 from flask_login import login_user, current_user, logout_user, login_required, LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+login_manager = LoginManager()
 login_manager.login_view = 'login'
 # login_manager.login_message_category = 'info'
-from portfolio import routes
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from portfolio.routes import main
+    app.register_blueprint(main)
+
+    return app
