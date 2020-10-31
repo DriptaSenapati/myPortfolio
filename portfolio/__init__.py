6b00c469e8d64f6959035ccc90db283a4b97c0ee
DriptaSenapati <driptasenapati97@gmail.com>
2020-10-31 20:11:19 +0530

    loginview added

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = 'main.login'
 # login_manager.login_message_category = 'info'
 
 
